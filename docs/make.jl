2a0b03b7defbeeb702da1054b1693b4d10da179f
waltergu <waltergu1989@gmail.com>
2018-10-12 10:49:33 +0800

    Near there.

--- docs/make.jl
+++ docs/make.jl
@@ -4,29 +4,39 @@ using Documenter
 using Hamiltonian,Hamiltonian.Utilities.GoodQuantumNumber
 
 makedocs(
-#    format=     :html,
-    sitename=   "Hamiltonian.jl",
-    modules=    [Hamiltonian,Hamiltonian.Utilities,Hamiltonian.Utilities.GoodQuantumNumber],
-    clean=      false,
-    pages=      [
-                "Home"      =>  "index.md",
-                "Tutorial"  =>  [
-                        "tutorial/Unitcell Description.md",
-                        "tutorial/Engine App Interface.md",
-                        ],
-                "Manual"    =>  [
-                        "Utilities" =>  [
-                                "man/Utilities/Introduction.md",
-                                "man/Utilities/NamedVector.md",
-                                "man/Utilities/GoodQuantumNumber.md",
+    format=             :html,
+    clean=              false,
+    sitename=           "Hamiltonian.jl",
+    modules=            [Hamiltonian,Hamiltonian.Utilities,Hamiltonian.Utilities.GoodQuantumNumber],
+    pages=              [
+                        "Home"      =>  "index.md",
+                        "Tutorial"  =>  [
+                                "tutorial/Unitcell Description.md",
+                                "tutorial/Engine App Interface.md",
+                                ],
+                        "Manual"    =>  [
+                                "Utilities" =>  [
+                                        "man/Utilities/Introduction.md",
+                                        "man/Utilities/NamedVector.md",
+                                        "man/Utilities/GoodQuantumNumber.md",
+                                        ],
                                 ],
                         ],
-                ]
+    html_canonical=     "https://quantum-many-body.github.io/Hamiltonian.jl/latest/"
 )
 
+# deploydocs(
+#     deps=       Deps.pip("pygments","mkdocs","python-markdown-math"),
+#     repo=       "github.com/Quantum-Many-Body/Hamiltonian.jl.git",
+#     julia=      "1.0",
+#     osname=     "linux"
+# )
+
 deploydocs(
-    deps=       Deps.pip("pygments","mkdocs","python-markdown-math"),
     repo=       "github.com/Quantum-Many-Body/Hamiltonian.jl.git",
+    target=     "build",
     julia=      "1.0",
-    osname=     "linux"
+    osname=     "linux",
+    deps=       nothing,
+    make=       nothing,
 )
