58622ae795a8e9d1f340383090a3afe59302031f
waltergu <waltergu1989@gmail.com>
2019-04-28 22:38:14 +0800

    Update the docs config to use the latest Documenter.

--- docs/make.jl
+++ docs/make.jl
@@ -1,55 +1,57 @@
 push!(LOAD_PATH,"../src/")
 
 using Documenter
+using DocumenterTools
 using QuantumLattices
 
 makedocs(
-    format=             :html,
-    clean=              false,
-    sitename=           "QuantumLattices.jl",
-    pages=              [
-                        "Home"      =>  "index.md",
-                        "Tutorial"  =>  [
-                                "tutorial/UnitcellDescription.md",
-                                "tutorial/EngineAppInterface.md",
-                                ],
-                        "Manual"    =>  [
-                                "man/Interfaces.md",
-                                "Prerequisites" => [
-                                        "man/Prerequisites/Introduction.md",
-                                        "man/Prerequisites/TypeTraits.md",
-                                        "man/Prerequisites/Factories.md",
-                                        "man/Prerequisites/CompositeStructures.md",
-                                        "man/Prerequisites/SimpleTrees.md",
-                                        "man/Prerequisites/NamedVectors.md",
-                                        ],
-                                "Mathematics" => [
-                                        "man/Mathematics/Introduction.md",
-                                        "man/Mathematics/Combinatorics.md",
-                                        "man/Mathematics/VectorSpaces.md",
-                                        "man/Mathematics/AlgebraOverFields.md",
-                                        "man/Mathematics/QuantumNumbers.md",
-                                        ],
-                                "Essentials" => [
-                                        "man/Essentials/Introduction.md",
-                                        "man/Essentials/Spatials.md",
-                                        "man/Essentials/DegreesOfFreedom.md",
-                                        "man/Essentials/Terms.md",
-                                        "man/Essentials/FockPackage.md",
-                                        "man/Essentials/SpinPackage.md",
-                                        "man/Essentials/Extensions.md",
-                                        ],
-                                ],
+    format=     Documenter.HTML(
+                    prettyurls= !("local" in ARGS),
+                    canonical=  "https://quantum-many-body.github.io/QuantumLattices.jl/latest/",
+                    assets=     ["assets/favicon.ico"],
+                    analytics=  "UA-89508993-1",
+                    ),
+    clean=      false,
+    sitename=   "QuantumLattices.jl",
+    pages=      [
+                "Home"      =>  "index.md",
+                "Tutorial"  =>  [
+                    "tutorial/UnitcellDescription.md",
+                    "tutorial/EngineAppInterface.md",
                         ],
-    html_canonical=     "https://quantum-many-body.github.io/QuantumLattices.jl/latest/",
-    assets=             ["assets/favicon.ico"]
+                "Manual"    =>  [
+                    "man/Interfaces.md",
+                    "Prerequisites" => [
+                        "man/Prerequisites/Introduction.md",
+                        "man/Prerequisites/TypeTraits.md",
+                        "man/Prerequisites/Factories.md",
+                        "man/Prerequisites/CompositeStructures.md",
+                        "man/Prerequisites/SimpleTrees.md",
+                        "man/Prerequisites/NamedVectors.md",
+                        ],
+                    "Mathematics" => [
+                        "man/Mathematics/Introduction.md",
+                        "man/Mathematics/Combinatorics.md",
+                        "man/Mathematics/VectorSpaces.md",
+                        "man/Mathematics/AlgebraOverFields.md",
+                        "man/Mathematics/QuantumNumbers.md",
+                        ],
+                    "Essentials" => [
+                        "man/Essentials/Introduction.md",
+                        "man/Essentials/Spatials.md",
+                        "man/Essentials/DegreesOfFreedom.md",
+                        "man/Essentials/Terms.md",
+                        "man/Essentials/FockPackage.md",
+                        "man/Essentials/SpinPackage.md",
+                        "man/Essentials/Extensions.md",
+                        ],
+                    ],
+                ]
 )
 
 deploydocs(
     repo=       "github.com/Quantum-Many-Body/QuantumLattices.jl.git",
     target=     "build",
-    julia=      "1.1",
-    osname=     "linux",
     deps=       nothing,
     make=       nothing,
 )
